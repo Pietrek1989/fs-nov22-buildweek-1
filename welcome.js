aedd784b87df1cde222221eed8cf9e0904cd1cbc
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2022-12-12 16:47:02 +0100

    Delete welcome.js

--- welcome.js
+++
@@ -1,8 +0,0 @@
-function triggerFeedback() {
-  let agreedToTestRules = document.getElementById("quiz-agreement").checked;
-  if (agreedToTestRules === true) {
-    window.location.href = "feedback.html";
-  } else {
-    alert("Please agree to the Benchmark Rules in order to continue");
-  }
-}
